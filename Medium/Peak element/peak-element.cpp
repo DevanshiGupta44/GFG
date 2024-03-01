58fc856323380214e0111ca1f7e4a6211ea01050
Devanshi Gupta <121246424+DevanshiGupta44@users.noreply.github.com>
2024-03-01 16:48:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Peak element/peak-element.cpp
+++ Medium/Peak element/peak-element.cpp
@@ -16,18 +16,16 @@ class Solution
     {
        // Your code here
        int left = 0;
-        int right = n - 1;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-            if (arr[mid] > arr[mid + 1]) {
+       int right = n-1;
+        while(left<right){
+            int mid = left + (right-left) / 2;
+            if(arr[mid]>arr[mid + 1]){
                 right = mid;
             } else {
                 left = mid + 1;
+                
             }
         }
-    
         return left;
     }
 };
