e89680a0a9137e4cf67eb6b079770e1ff6a17f6a
Devanshi Gupta <121246424+DevanshiGupta44@users.noreply.github.com>
2024-06-05 09:50:42 +0530

    Updated Solution - GfG to GitHub

--- Basic/Check If Circular Linked List/check-if-circular-linked-list.cpp
+++ Basic/Check If Circular Linked List/check-if-circular-linked-list.cpp
@@ -75,15 +75,14 @@ struct Node
 bool isCircular(Node *head)
 {
    // Your code here
-   if(head==NULL){
-        return true;
-    }
-    Node *temp=head->next;
-    while(temp!=NULL && temp!=head){
-        temp=temp->next;
-    }
-    if(temp==head){
-        return true;
-    }
-        return false;
+   Node* slow = head;
+   Node* fast = head;
+   while(fast!=NULL && fast->next!=NULL){
+       slow=slow->next;
+       fast=fast->next->next;
+       if(slow==fast){
+           return 1;
+       }
+   }
+   return 0;
 }
