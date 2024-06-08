3140dd81fcae1ab486b61a4a675ba8224f399e21
Devanshi Gupta <121246424+DevanshiGupta44@users.noreply.github.com>
2024-06-08 09:23:06 +0530

    Updated Solution - GfG to GitHub

--- Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -5,32 +5,36 @@ using namespace std;
 
 // } Driver Code Ends
 
-
-
 class Solution
 {
     public:
     //Function to check if brackets are balanced or not.
     bool ispar(string x)
     {
-        stack<char>st;
+        // Your code here
+        stack<char> st;
         for(int i=0;i<x.size();i++){
-            if(x[i]=='(' or x[i]=='[' or x[i]=='{'){
+            if(x[i] == '(' || x[i] == '{' || x[i] == '['){
                 st.push(x[i]);
             }
-            else {
-                if(st.empty()) return false;
-                if((st.top()=='('&& x[i]==')') || (st.top()=='['&& x[i]==']') || (st.top()=='{'&& x[i]=='}')){
-                st.pop();
-              }
-              else {
-                  
-                  return false;
-              }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if(st.top()=='(' && x[i] == ')' || st.top()=='{' && x[i] == '}' || st.top()=='[' && x[i] == ']'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
             }
         }
-        if(st.empty()) return true;
-        else return false;
+        if(st.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 };
