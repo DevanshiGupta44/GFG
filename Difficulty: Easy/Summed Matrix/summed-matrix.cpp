15ec8cb76429949b2c07f863935d3e8bda90489f
Devanshi Gupta <121246424+DevanshiGupta44@users.noreply.github.com>
2024-06-24 10:30:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        // code here
+        n=n+1;
+        if(n>=q){
+            return max(0ll,n-(n-q)-1);
+        }
+        else{
+            return max(0ll,n-(q-n)-1);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
