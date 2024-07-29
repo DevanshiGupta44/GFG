50de740d39fa64fc0a50201a1443e5e66686ce4c
Devanshi Gupta <121246424+DevanshiGupta44@users.noreply.github.com>
2024-07-29 19:04:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -2,24 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-//User function template for C++
-class Solution{
-public:
-	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
-	    // code here
-	    int index = -1;
-	    int sum = 0;
-	    for(int i=0;i<n;i++){
-	        int temp = accumulate(arr[i].begin(),arr[i].end(),0);
-	        if(temp>sum){
-	            sum = temp;
-	            index = max(index,i);
-	        }
-	    }
-	    return index;
-	}
 
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int> > &arr) {
+        int maxOnesIndx = -1;
+        int maxOnes = 0;
+        int n = arr.size(), m = arr[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(arr[i][j]==1){
+                    int ones = m - j;
+                    if(ones>maxOnes){
+                        maxOnes = ones;
+                        maxOnesIndx = i;
+                    }
+                    break;
+                }
+            }
+        }
+        return maxOnesIndx;
+    }
 };
 
 //{ Driver Code Starts.
@@ -29,14 +35,14 @@ int main() {
     while (t--) {
         int n, m;
         cin >> n >> m;
-        vector< vector<int> > arr(n,vector<int>(m));
+        vector<vector<int> > arr(n, vector<int>(m));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                cin>>arr[i][j];
+                cin >> arr[i][j];
             }
         }
         Solution ob;
-        auto ans = ob.rowWithMax1s(arr, n, m);
+        auto ans = ob.rowWithMax1s(arr);
         cout << ans << "\n";
     }
     return 0;
